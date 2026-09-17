1efafb38b25b9b0d46788a6a21ff24ea67561f26
Yulia Molodykh <153188400+chronattx@users.noreply.github.com>
2026-09-17 14:57:27 +0300

    lab1.0

---
+++ task1.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <windows.h>
+
+int *find_min(int *begin, int *end) {
+    int *min = begin;
+
+    for (int *ptr = begin; ptr < end; ptr++) if (*ptr < *min) min = ptr;
+
+    return min;
+}
+
+
+int *find_max(int *begin, int *end) {
+    int *max = begin;
+    
+    for (int *ptr = begin; ptr < end; ptr++) if (*ptr > *max) max = ptr;
+
+    return max;
+}
+
+
+long long sum_between(const int *first, const int *second) {
+    long long total = 0;
+    if (first > second) {
+        for (const int *ptr = second + 1; ptr < first; ptr++) {
+            total += *ptr;
+        }
+    } else {
+        for (const int *ptr = first + 1; ptr < second; ptr++) {
+            total += *ptr;
+        }
+    }
+    return total;
+}
+
+
+size_t find_longest_increasing(int *begin, int *end, int **sequence_begin) {
+    size_t max_len = 1;
+    size_t curr_len = 1;
+    *sequence_begin = begin;
+
+    for (int *ptr = begin; ptr + 1 < end; ptr++) {
+        if (*ptr < *(ptr + 1)) curr_len++;
+        else curr_len = 1;
+        if (curr_len > max_len) {
+            max_len = curr_len;
+            *sequence_begin = ptr - curr_len + 2;  
+        }
+    }
+    return max_len;
+
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void reverse_range(int *begin, int *end) {
+    int *begin_ptr = begin, *end_ptr = end - 1;
+    while (end_ptr - begin > 0) {
+        swap(begin_ptr, end_ptr);
+        begin_ptr++;
+        end_ptr--;
+    }
+}
+
+void print_array_nekruto(const int *begin, const int *end) {
+    for (int i = 0; i < (int) (end - begin); i++) {
+        printf("%d ", *(begin + i));
+    }
+    printf("\n");
+}
+
+void print_array(const int *begin, const int *end) {
+    for (const int *ptr = begin; ptr < end; ptr++) {
+        printf("%d ", *ptr);
+    }
+    printf("\n");
+}
+
+
+int main(void) {
+    SetConsoleOutputCP(65001);
+    int arr[1000], n, written = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) written += scanf("%d", arr + i);
+    
+    if (written != n) {
+        printf("didn't get the digits correctly");
+        return 1;
+    }
+
+    int *min_ptr = find_min(arr, arr + n);
+    int *max_ptr = find_max(arr, arr + n);
+    long long sum_min_max = sum_between(min_ptr, max_ptr);
+
+    int len_between;
+    if (min_ptr > max_ptr) len_between = (int) (min_ptr - max_ptr);
+    else len_between = (int) (max_ptr - min_ptr);
+
+    int *sequence_begin;
+    size_t max_len = find_longest_increasing(arr, arr + n, &sequence_begin);
+
+    printf("Исходный массив:\n");
+    print_array(arr, arr + n);
+    printf("Минимум: %d\n", *min_ptr);
+    printf("Позиция минимума: %d\n", min_ptr - arr);
+    printf("Максимум: %d\n", *max_ptr);
+    printf("Позиция максимума: %d\n", max_ptr - arr);
+    printf("Расстояние между минимумом и максимумом: %d\n", len_between);
+    printf("Сумма элементов между минимумом и максимумом: %d\n", sum_min_max);
+    printf("\n");
+    
+    printf("Самый длинный возрастающий участок:\n");
+    print_array(sequence_begin, sequence_begin + max_len);
+    printf("Длина: %d", max_len);
+    printf("\n");
+
+    reverse_range(sequence_begin, sequence_begin + max_len);
+    printf("Массив после разворота выбранного участка:\n");
+    print_array(arr, arr + n);
+
+    return 0;
+}
---
+++ task2.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <windows.h>
+
+/* меняет значения переменных без 3ей переменной */
+void swap(int *a, int *b) {
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
+/* выводит матрицу в табличном виде */
+void print_matrix(size_t rows, size_t cols, const int matrix[rows][cols]) {
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/* возвращает указатель на макс значение и записывает строчку и колонку по адресам указателей */
+const int *find_matrix_max(size_t rows, size_t cols, const int matrix[rows][cols], size_t *max_row, size_t *max_col) {
+    const int *ptr_max = matrix[0];
+    for (size_t i = 0; i < rows; i++) {
+        const int * row = matrix[i];
+        for (const int * ptr = row; (size_t)(ptr - row) < cols; ptr++) {
+            if (*ptr > *ptr_max) {
+                ptr_max = ptr;
+                *max_row = i;
+                *max_col = (size_t) (ptr - row);
+            }
+        }
+    }
+    return ptr_max;
+}
+
+/*среднее арифметическое строки*/
+double row_average(size_t cols, const int *row) {
+    long long sum = 0;
+    for (const int *ptr = row; (size_t) (ptr - row) < cols; ptr++) sum += *ptr;
+    return (double) sum / (double) cols;
+}
+
+/* среднее арифметическое матрицы */
+double matrix_average(size_t rows, size_t cols, const int matrix[rows][cols]) {
+    double average = 0;
+    short flag = 0;
+    for (size_t i = 0; i < rows; i++) {
+        const int * row = matrix[i];
+        average += row_average(cols, row);
+    }
+    return average / (double) rows;
+}
+
+/* количество значений выше среднего */
+size_t count_above_average(size_t rows, size_t cols, const int matrix[rows][cols], double average) {
+    size_t counter = 0;
+    for (size_t i = 0; i < rows; i++) {
+        const int * row = matrix[i];
+        for (const int * ptr = row; (size_t)(ptr - row) < cols; ptr++) {
+            if ((double)*ptr > average) counter++;
+        }
+    }
+    return counter;
+}
+
+/* строка с наибольшим средним значением */
+size_t hottest_row(size_t rows, size_t cols, const int matrix[rows][cols]) {
+    size_t hot_row = 0;
+    double hot_average = row_average(cols, matrix[0]);
+
+    for (size_t i = 1; i < rows; i++) {
+        double local_average = row_average(cols, matrix[i]);
+        if (local_average > hot_average){
+            hot_average = local_average;
+            hot_row = i;
+        }
+    }
+    return hot_row;
+}
+
+/* заполняет строку её средним значением */
+void fill_row_with_average(size_t cols, int *row) {
+    int average = (int) row_average(cols, row);
+    for (int * ptr = row; (size_t) (ptr - row) < cols; ptr++) {
+        *ptr = average;
+    }
+}
+
+/* находит максимальную разницу между соседями */
+int max_neighbor_difference(size_t rows, size_t cols, const int matrix[rows][cols]) {
+    int max_dif = 0;
+    for (size_t i = 0; i < rows; i++) {
+        const int * row = matrix[i];
+        for (const int * ptr = row; (size_t)(ptr - row) < cols - 1; ptr++) {
+            if (*(ptr + 1) - *ptr > max_dif) max_dif = *(ptr + 1) - *ptr;
+            if (*ptr - *(ptr + 1) > max_dif) max_dif = *ptr - *(ptr + 1);
+        }
+    }
+    return max_dif;
+}
+
+int main(void) {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    
+    size_t rows, cols;
+    int got_digits = 0;
+    got_digits += scanf("%d%d", &rows, &cols);
+    
+    if (got_digits != 2) {
+        printf("не получили числа верно");
+        return 0;
+    }
+
+    int temperatures[rows][cols];
+
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            got_digits += scanf("%d", &temperatures[i][j]);
+        }
+    }
+
+    if (got_digits != 2 + rows * cols) {
+        printf("не получили числа верно");
+        return 0;
+    }
+    
+    printf("Исходная карта:\n");
+    print_matrix(rows, cols, temperatures);
+    printf("\n");
+
+    const int *ptr_max;
+    size_t max_col, max_row;
+    ptr_max = find_matrix_max(rows, cols, temperatures, &max_row, &max_col);
+    printf("Максимальная температура: %d\nСтрока: %d\nСтолбец: %d\n", *ptr_max, max_row, max_col);
+
+    double average = matrix_average(rows, cols, temperatures);
+
+    printf("Средняя температура карты: %.2f\nКоличество значений выше среднего: %d\n", average, count_above_average(rows, cols, temperatures, average));
+    size_t i = hottest_row(rows, cols, temperatures);
+    printf("Строка с максимальным средним: %d\nСреднее выбранной строки: %.2f\nКарта после замены выбранной строки:\n", i, row_average(cols, temperatures[i]));
+    fill_row_with_average(cols, temperatures[i]);
+    print_matrix(rows, cols, temperatures);
+    printf("Максимальная разница соседних температур: %d\n", max_neighbor_difference(rows, cols, temperatures));
+
+    return 0;
+}
